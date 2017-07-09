78175e93f49f4e578eec2e6e9187545d6a8ae873
mask0 <takashi.higasa@gmail.com>
2017-07-09 15:24:58 +0900

    XY_point print terminal

---
+++ access_XY/main.cpp
@@ -0,0 +1,46 @@
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+struct mouseParam{
+  int x;
+  int y;
+  int event;
+  int flags;
+};
+
+void CallBackFunc(int eventType, int x, int y, int flags, void* userdata){
+  mouseParam *ptr = static_cast<mouseParam*> (userdata);
+  ptr->x = x;
+  ptr->y = y;
+  ptr->event = eventType;
+  ptr->flags = flags;
+}
+    
+
+
+int main(void){
+  mouseParam mouseEvent;
+  
+  Mat input_image =imread("lena.png");
+  String showing_name ="input";
+  imshow(showing_name, input_image);
+
+  setMouseCallback(showing_name, CallBackFunc, &mouseEvent);
+  while(1){
+    waitKey(1);
+    if(mouseEvent.event == EVENT_MOUSEMOVE){
+      cout << mouseEvent.x << "," << mouseEvent.y << endl;
+    }
+
+    else if(mouseEvent.event == EVENT_RBUTTONDOWN){
+      break;
+    }
+  }
+  return 0;
+}
