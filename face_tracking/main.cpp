ac323e5631ec35500e7ba845bf79f7fff19a183e
mask0 <takashi.higasa@gmail.com>
2017-08-08 12:54:01 +0900

    input number of people for image

--- face_tracking/main.cpp
+++ face_tracking/main.cpp
@@ -1,8 +1,12 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 using namespace cv;
+
+int face_num =0;
 
 Mat detectFaceInImage(Mat &image, string &cascade_file){
   CascadeClassifier cascade;
@@ -13,15 +17,28 @@ Mat detectFaceInImage(Mat &image, string &cascade_file){
 
   for(int i =0; i < faces.size(); i++){
     rectangle(image, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y+faces[i]. height), Scalar(0, 200, 0), 1, CV_AA);
+    face_num++;
   }
-
+  
   return image;
 }
 
+void IntToString(char *str, int number){
+  sprintf(str, "%d", number);
+}
 int main(int argc, char const *argv[]){
   Mat image = imread(argv[1]);
   string filename = argv[2];
+  char people[] = "people";
+  char sNum[32], str;
+  
   Mat detectFaceImage = detectFaceInImage(image, filename);
+
+  //input number of people for image.
+  IntToString(sNum, face_num);
+  strcat(sNum, people);
+  putText(detectFaceImage, sNum, Point(50, 50), FONT_HERSHEY_TRIPLEX, 1, Scalar(255,255,255), 1, CV_AA);
+
   imshow("detect_face", detectFaceImage);
   waitKey(0);
 
